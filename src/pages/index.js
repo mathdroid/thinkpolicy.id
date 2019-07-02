1302b5829d629f1e26e9b1725383581f928f2b94
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-07-02 20:40:13 +0700

    Add basic form

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,38 @@ export default () => {
     <div>
       <h1>{headline}</h1>
       <p>{subheadline}</p>
-      <input placeholder="nama@contoh.com" />
-      <button>{cta}</button>
+      <form
+        name="signup"
+        method="POST"
+        netlify-honeypot="contact_me_by_fax_only"
+        data-netlify="true"
+      >
+        <input
+          className="hidden"
+          type="checkbox"
+          name="contact_me_by_fax_only"
+          value="1"
+          tabindex="-1"
+          autocomplete="off"
+        />
+        <label>
+          Email: <input type="text" name="email" />
+        </label>
+        <br />
+        <label>
+          Nama: <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Tahun Kelahiran: <input type="text" name="birthyear" />
+        </label>
+        <br />
+        <label>
+          Organisasi: <input type="text" name="organization" />
+        </label>
+        <br />
+        <button type="submit">{cta}</button>
+      </form>
     </div>
   );
 };
