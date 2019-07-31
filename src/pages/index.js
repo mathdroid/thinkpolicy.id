643bb9ac506264b77a8f87ed521923d49298a3a2
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-07-31 18:50:02 +0700

    change post method

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,14 +17,16 @@ const signupSchema = Yup.object().shape({
     .required(`We'd love to know your age`),
   organization: Yup.string().required(
     `What organization are you in? If not applicable, type 'Personal'`
-  )
+  ),
+  contact_me_by_fax_only: Yup.boolean()
 });
 
 const initialValues = {
   email: "",
   name: "",
   birthyear: "",
-  organization: ""
+  organization: "",
+  contact_me_by_fax_only: true
 };
 
 const encode = data => {
@@ -33,91 +35,121 @@ const encode = data => {
     .join("&");
 };
 
-const handleSubmit = values => {
-  const encoded = encode({ "form-name": "signup", ...values });
-  fetch(`/?${encoded}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encoded
-  })
-    .then(() => alert("Success!"))
-    .catch(error => alert(error));
-
-  // e.preventDefault();
-};
-
 export default () => {
   const { headline, subheadline, cta } = useSiteData();
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [submittedName, setSubmittedName] = useState("");
+  const handleSubmit = values => {
+    const encoded = encode({ "form-name": "signup", ...values });
+    fetch(`/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encoded
+    })
+      .then(() => {
+        setSubmittedName(values.name);
+        setHasSubmitted(true);
+      })
+      .catch(error => alert(error));
+
+    // e.preventDefault();
+  };
+
   return (
     <>
       <h1>{headline}</h1>
       <p>{subheadline}</p>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={signupSchema}
-        onSubmit={async (values, actions) => {
-          // alert(JSON.stringify(values));
-          const data = await handleSubmit(values);
-          alert(JSON.stringify(data));
-          actions.setSubmitting(false);
-        }}
-      >
-        <Form>
-          <Field
-            name="email"
-            render={({ field }) => (
-              <label>
-                <h3 style={{ marginBottom: 0 }}>Email: </h3>
-                <input
-                  type="text"
-                  {...field}
-                  placeholder="saya@perusahaan.com"
-                />
-              </label>
-            )}
-          />
-          <ErrorMessage name="email" />
+      {hasSubmitted ? (
+        <h2>
+          Thanks for submitting, {submittedName}! We will reach out to you.
+        </h2>
+      ) : (
+        <Formik
+          initialValues={initialValues}
+          validationSchema={signupSchema}
+          onSubmit={async (values, actions) => {
+            // alert(JSON.stringify(values));
+            await handleSubmit(values);
+            actions.setSubmitting(false);
+          }}
+          render={formikProps => (
+            <form
+              name="signup"
+              netlify-honeypot="contact_me_by_fax_only"
+              data-netlify="true"
+              onReset={formikProps.handleReset}
+              onSubmit={formikProps.handleSubmit}
+            >
+              <Field
+                name="contact_me_by_fax_only"
+                render={({ field }) => (
+                  <input
+                    className="hidden"
+                    type="checkbox"
+                    name="contact_me_by_fax_only"
+                    tabindex="-1"
+                    autocomplete="off"
+                    {...field}
+                  />
+                )}
+              />
+              <Field
+                name="email"
+                render={({ field }) => (
+                  <label>
+                    <h3 style={{ marginBottom: 0 }}>Email: </h3>
+                    <input
+                      type="text"
+                      {...field}
+                      placeholder="saya@perusahaan.com"
+                    />
+                  </label>
+                )}
+              />
+              <ErrorMessage name="email" />
 
-          <Field
-            name="name"
-            render={({ field }) => (
-              <label>
-                <h3 style={{ marginBottom: 0 }}>Nama: </h3>
-                <input type="text" {...field} placeholder="Odi" />
-              </label>
-            )}
-          />
-          <ErrorMessage name="name" />
+              <Field
+                name="name"
+                render={({ field }) => (
+                  <label>
+                    <h3 style={{ marginBottom: 0 }}>Nama: </h3>
+                    <input type="text" {...field} placeholder="Odi" />
+                  </label>
+                )}
+              />
+              <ErrorMessage name="name" />
 
-          <Field
-            name="birthyear"
-            render={({ field }) => (
-              <label>
-                <h3 style={{ marginBottom: 0 }}>Tahun Kelahiran: </h3>
-                <input type="text" {...field} placeholder="1990" />
-              </label>
-            )}
-          />
-          <ErrorMessage name="birthyear" />
+              <Field
+                name="birthyear"
+                render={({ field }) => (
+                  <label>
+                    <h3 style={{ marginBottom: 0 }}>Tahun Kelahiran: </h3>
+                    <input type="text" {...field} placeholder="1990" />
+                  </label>
+                )}
+              />
+              <ErrorMessage name="birthyear" />
 
-          <Field
-            name="organization"
-            render={({ field }) => (
-              <label>
-                <h3 style={{ marginBottom: 0 }}>Organisasi: </h3>
-                <input type="text" {...field} placeholder="Personal" />
-              </label>
-            )}
-          />
-          <ErrorMessage name="organization" />
-          <button type="submit">{cta}</button>
-        </Form>
-      </Formik>
+              <Field
+                name="organization"
+                render={({ field }) => (
+                  <label>
+                    <h3 style={{ marginBottom: 0 }}>Organisasi: </h3>
+                    <input type="text" {...field} placeholder="Personal" />
+                  </label>
+                )}
+              />
+              <ErrorMessage name="organization" />
+              <button type="submit" disabled={formikProps.isSubmitting}>
+                {cta}
+              </button>
+            </form>
+          )}
+        />
+      )}
       {/* <form
         name="signup"
         method="POST"
-        netlify-honeypot="contact_me_by_fax_only"
-        data-netlify="true"
       >
         <input
           className="hidden"
