5ad08161443c39e5e0e3a2cdd054e1017d67f108
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-07-31 18:55:04 +0700

    useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSiteData } from "react-static";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import fetch from "isomorphic-unfetch";
 
